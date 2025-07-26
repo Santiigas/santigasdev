9e4d703b2106ffdb500cf4b2bf388310807556a2
Santiago Oliveira <santiagosof@gmail.com>
2025-07-26 15:20:26 -0300

    feat: Estrutura do código mais mediaqueries

--- santigas.dev/src/pages/home-page/portfolio.tsx
+++ santigas.dev/src/pages/home-page/portfolio.tsx
@@ -1,4 +1,5 @@
 import './portfolio.css'
+import './responsive.css';
 import Logosantigas from '../../images/logo.png'
 
 function portfolio() {
@@ -6,25 +7,26 @@ function portfolio() {
     return (
         <div className='body'>
             <section className='general-presentation'>
-            <div className='presentation'>
-                 <img className='logoSantigas' src={Logosantigas} alt="" />
-                 <h2 className='main-title'>Developer full-stack </h2>
-                 <h2 className='secondary-title'>JavaScrpit -  Node - React  -  MySql  -  Python</h2>
-            </div>
-            <div className="navbar">
-                <li className='navigation'>Sobre</li>
-                <li className='navigation'>Skills</li>
-                <li className='navigation'>Experience</li>
-                <li className='navigation'>Education</li>
-                <li className='navigation'>Contact</li>
-            </div>
+                <div className='presentation'>
+                    <img className='logoSantigas' src={Logosantigas} alt="" />
+                    <h2 className='main-title'>Developer full-stack </h2>
+                    <h2 className='secondary-title'>JavaScrpit -  Node - React  -  MySql  -  Python</h2>
+                </div>
+                <div className="navbar">
+                    <li className='navigation'>Sobre</li>
+                    <li className='navigation'>Skills</li>
+                    <li className='navigation'>Experience</li>
+                    <li className='navigation'>Education</li>
+                    <li className='navigation'>Contact</li>
+                </div>
             </section>
+
             <section className='about-me'>
                 <div className='who-am-i'>
                     <div className='card-about-me'>
                         <div className='card-about-me-1'>
                             <h2 className='tilte-about-me'>About me</h2>
-                            
+
                             
                         </div>
                         <div className='card-about-me-2'>
@@ -33,12 +35,48 @@ function portfolio() {
                     </div>
                 </div>
             </section>
+
             <section className='professional-experiences'>
-
+                <div className='experiences_general'> 
+                    <div className='experiences'></div>
+                    <div className='experiences'></div>
+                    <div className='experiences'></div>
+                </div>
             </section>
-            <section className='education-and-skills'>
 
+            <section className='courses-and-education'>
+                <div className='educations-general'>
+                    <div className='educations'>
+                        <div className='institution'>
+
+                        </div>
+                        <div className='description-institution'>
+
+                        </div>
+                    </div>
+                    <div className='educations'>
+                        <div className='institution'>
+
+                        </div>
+                        <div className='description-institution'>
+
+                        </div>                        
+                    </div>
+                    <div className='educations'>
+                        <div className='institution'>
+
+                        </div>
+                        <div className='description-institution'>
+
+                        </div>
+                    </div>
+                </div>
             </section>
+
+            <section className='skills'>
+                
+            </section>
+
             <section className='projects-and-collaborations'>
 
             </section>
