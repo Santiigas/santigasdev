05ecb09f9d3093a27176a7b840784dda035b0310
Santiago Oliveira <santiagosof@gmail.com>
2025-08-02 11:36:11 -0300

    feat: About me, mais atualizações

--- santigas.dev/src/pages/home-page/portfolio.tsx
+++ santigas.dev/src/pages/home-page/portfolio.tsx
@@ -26,11 +26,28 @@ function portfolio() {
                     <div className='card-about-me'>
                         <div className='card-about-me-1'>
                             <h2 className='tilte-about-me'>About me</h2>
-
-                            
+                            <p>Sou brasileiro, entusiasta de tecnologia, audiovisual e musculação. Atuo no desenvolvimento de sistemas com foco em 
+                            APIs e arquitetura web, e tenho grande interesse por UI/UX design — criando interfaces modernas, 
+                            intuitivas e centradas na experiência do usuário.<br></br><br></br>
+                            Sou o criador da ferramenta LayoutColors.com, voltada para designers e desenvolvedores que buscam testar combinações 
+                            de cores em layouts responsivos. Atualmente, venho explorando o universo da criação de jogos, unindo lógica de programação à criatividade visual.</p>                            
                         </div>
                         <div className='card-about-me-2'>
-
+                            <div className='card-information'>
+                                <div className='photo-santi'></div>
+                                <div className='info_santi'>
+                                    <h2>Santiago Oliveira</h2>
+                                    <p>Software developer</p>
+                                    <p>Idealle Sistemas</p>
+                                    <p>Estácio</p>
+                                </div>
+                            </div>
+                            <div className='contact_santi'>
+                                Aracaju, Sergipe, Brasil
+                            </div>
+                            <div className='contact_santi'>
+                                santiagosof@gmail.com
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,33 +55,73 @@ function portfolio() {
 
             <section className='professional-experiences'>
                 <div className='experiences_general'> 
-                    <div className='experiences'></div>
-                    <div className='experiences'></div>
-                    <div className='experiences'></div>
+                    <div className='experiences'>
+                        <div className='work_enterprise'>
+                            <div className='work_logo'>
+
+                            </div>
+                            <div className='work_information'>
+                                <h2>IDEALLE SISTEMAS</h2>
+                                <h3>Tempo de serviço: de 02/02/2003</h3>
+                            </div>
+                        </div>
+                        <div className='description_work'>
+
+                        </div>
+                    </div>
+                    <div className='experiences'>
+                        <div className='work_enterprise'>
+                            <div className='work_logo'>
+
+                            </div>
+                            <div className='work_information'>
+
+                            </div>
+                        </div>
+                        <div className='description_work'>
+
+                        </div>
+                    </div>
+                    <div className='experiences'>
+                        <div className='work_enterprise'>
+                            <div className='work_logo'>
+
+                            </div>
+                            <div className='work_information'>
+
+                            </div>
+                        </div>
+                        <div className='description_work'>
+
+                        </div>
+                    </div>
                 </div>
             </section>
 
             <section className='courses-and-education'>
                 <div className='educations-general'>
-                    <div className='educations'>
+                    <div className='educations' id='educations1'>
                         <div className='institution'>
-
+                            <div className='image-institution'>
+                            </div>
                         </div>
                         <div className='description-institution'>
 
                         </div>
                     </div>
-                    <div className='educations'>
+                    <div className='educations' id='educations2'> 
                         <div className='institution'>
-
+                            <div className='image-institution'>
+                            </div>
                         </div>
                         <div className='description-institution'>
 
                         </div>                        
                     </div>
-                    <div className='educations'>
+                    <div className='educations' id='educations3'>
                         <div className='institution'>
-
+                            <div className='image-institution'>
+                            </div>
                         </div>
                         <div className='description-institution'>
 
